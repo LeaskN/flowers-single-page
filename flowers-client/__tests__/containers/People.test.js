c86928fc361f8b2d3a713cd74771d8d44c2ba6a9
Nicholas Leask <leask.nicholas@gmail.com>
2018-10-02 13:16:06 -0400

    import error

--- flowers-client/__tests__/containers/People.test.js
+++ flowers-client/__tests__/containers/People.test.js
@@ -1,7 +1,7 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// import toJson from 'enzyme-to-json';
-// import configureStore from 'redux-mock-store';
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import configureStore from 'redux-mock-store';
 import People from '../src/containers/People';
 
 test('Fake Test', () => {
